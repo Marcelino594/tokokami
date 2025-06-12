fccc2f91edae34ce19379c12424d7dc9256cea43
yuanardin <ardinyuan00@gmail.com>
2025-06-12 12:55:19 +0700

    commit:models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
-}
+    use HasFactory;
+
+    protected $fillable = [
+            'nama',
+            'harga',
+            'deskripsi',
+            'foto'
+    ];
+} 
