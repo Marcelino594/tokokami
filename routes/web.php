6f5a47c21b81c9d5a3b99157a35982e2434657cf
M Nauffal Raditya <nauffalraditya93@gmail.com>
2025-06-13 23:06:05 +0700

    commit:routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route :: get(' /dashboard', function() {
+    return view (' /dashboard');
+})->middleware({'auth', 'verified'})->name('dashboard');
+
+Route :: middleware('auth')->group(function () {
+    Route :: get ('products', [ProductController::class, 'index'])->name('products.index');
+    Route :: get ('/proflie', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route :: patch ('/proflie', [ProfileController::class, 'update'])->name('profile.update');
+    Route :: delete ('/proflie', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ , '/auth.php';
