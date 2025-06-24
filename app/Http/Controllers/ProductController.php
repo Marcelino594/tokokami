577d09c3f29ecee46b7f36176601b26514184e48
M Nauffal Raditya <nauffalraditya93@gmail.com>
2025-06-24 17:46:29 +0700

    commit:create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,4 +13,25 @@ class ProductController extends Controller
 
         return view('products.index', compact('products'));
     }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama' => 'required',
+            'harga' => 'required|numeric'
+        ]);
+
+        product::create([
+            'nama' => $request->nama,
+            'harga' => $request->harga,
+            'deskripsi' => $request->deskripsi
+        ]);
+
+        return redirect()->route('products.index')->with('success', 'Add Product Success');
+    }
 }
